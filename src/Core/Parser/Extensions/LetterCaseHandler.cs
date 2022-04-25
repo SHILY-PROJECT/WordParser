cb43412b217682031ab5dce24565c809a69dc2d7
Ilya Shimbuev <iliyarock@gmail.com>
2022-04-25 22:19:56 +0300

    -Изменен метод сортировки под рекорды

--- src/Core/Parser/Extensions/LetterCaseHandler.cs
+++ src/Core/Parser/Extensions/LetterCaseHandler.cs
@@ -2,11 +2,11 @@
 
 internal static class LetterCaseHandler
 {
-    public static IEnumerable<WordModel> ChangeCase(this IEnumerable<WordModel> words, LetterСaseEnum letterСase) => letterСase switch
+    public static IEnumerable<WordModel> ChangeCase(this IEnumerable<WordModel> words, LetterСase letterСase) => letterСase switch
     {
-        LetterСaseEnum.FirstLetterInUpper => words.Select(wm => new WordModel(wm.Word != null ? new string(wm.Word.Select((letter, index) => index == 0 ? char.ToUpper(letter) : char.ToLower(letter)).ToArray()) : string.Empty, wm.Quantity)),
-        LetterСaseEnum.AllLetterInUpper => words.Select(wm => new WordModel(wm.Word != null ? wm.Word.ToUpper() : string.Empty, wm.Quantity)),
-        LetterСaseEnum.AllLetterInLower => words.Select(wm => new WordModel(wm.Word != null ? wm.Word.ToLower() : string.Empty, wm.Quantity)),
-        _ or LetterСaseEnum.Default => words
+        LetterСase.FirstLetterInUpper   => words.Select(wm => wm with { Word = new string(wm.Word.Select((l, i) => i == 0 ? char.ToUpper(l) : char.ToLower(l)).ToArray()) }),
+        LetterСase.AllLetterInUpper     => words.Select(wm => wm with { Word = wm.Word.ToUpper() }),
+        LetterСase.AllLetterInLower     => words.Select(wm => wm with { Word = wm.Word.ToLower() }),
+        LetterСase.Default or _         => words
     };
 }
