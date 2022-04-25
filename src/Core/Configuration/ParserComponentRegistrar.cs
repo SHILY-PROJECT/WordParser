93d47b46a71937d79c84abf5c0cd877d21124e2b
Ilya Shimbuev <iliyarock@gmail.com>
2022-04-25 21:55:38 +0300

    -Проект переведен на DI -Добавлен класс и метод для регистрации служб парсера

---
+++ src/Core/Configuration/ParserComponentRegistrar.cs
@@ -0,0 +1,16 @@
+﻿namespace WordParser.Core.Configuration;
+
+internal static class ParserComponentRegistrar
+{
+    public static IServiceCollection AddParserComponents(this IServiceCollection services)
+    {
+        var wordParserProcessSettings = WordParserSettingsHandler.GetSettings();
+
+        services
+            .AddSingleton(s => wordParserProcessSettings)
+            .AddSingleton<WordParserMainForm>()
+            .AddSingleton<IWordParser, WordParserMain>();
+
+        return services;
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -5,9 +5,15 @@ internal static class Program
     [STAThread]
     public static void Main()
     {
+        var host = CreateHostBuilder(Array.Empty<string>()).Build();
+
         Application.SetHighDpiMode(HighDpiMode.SystemAware);
         Application.EnableVisualStyles();
         Application.SetCompatibleTextRenderingDefault(false);
-        Application.Run(new WordParserMainForm());
+        Application.Run(host.Services.GetRequiredService<WordParserMainForm>());
     }
+
+    private static IHostBuilder CreateHostBuilder(string[] args) => Host
+        .CreateDefaultBuilder(args)
+        .ConfigureServices(services => services.AddParserComponents());
 }
