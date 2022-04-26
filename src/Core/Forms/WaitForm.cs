64fc8c40c37215be48ccc2ced335e7c6b67d4829
Ilya Shimbuev <iliyarock@gmail.com>
2022-04-26 20:42:45 +0300

    -Добавлена тень и скругления краёв

--- src/Core/Forms/WaitForm.cs
+++ src/Core/Forms/WaitForm.cs
@@ -1,32 +1,112 @@
-﻿using System.Drawing;
-using System.Windows.Forms;
+﻿namespace WordParser.Core.Forms;
 
-namespace WordParser.Core.Forms
+internal partial class WaitForm : Form
 {
-    internal partial class WaitForm : Form
+    public WaitForm()
     {
-        internal WaitForm()
-        {
-            InitializeComponent();
-            HandleEventsForm();
-        }
+        InitializeComponent();
+        HandleEventsForm();
 
-        /// <summary>
-        /// Обработка событий формы.
-        /// </summary>
-        private void HandleEventsForm()
-        {
-            this.Load += (s, e)
-                => this.Location = new Point(Owner.Location.X + Owner.Width / 2 - this.Width / 2, Owner.Location.Y + Owner.Height / 2 - this.Height / 2);
-        }
+        Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 25, 25));
+    }
 
-        /// <summary>
-        /// Обводка формы.
-        /// </summary>
-        /// <param name="e"></param>
-        protected override void OnPaint(PaintEventArgs e)
+    private void HandleEventsForm()
+    {
+        this.Load += (s, e) => this.Location = new Point(
+            Owner.Location.X + Owner.Width / 2 - this.Width / 2,
+            Owner.Location.Y + Owner.Height / 2 - this.Height / 2);
+    }
+
+    #region Shadow of form & Border for form.
+    private const int WM_NCHITTEST = 0x84;
+    private const int HTCLIENT = 0x1;
+    private const int HTCAPTION = 0x2;
+
+    private bool m_aeroEnabled;
+
+    private const int CS_DROPSHADOW = 0x00020000;
+    private const int WM_NCPAINT = 0x0085;
+    private const int WM_ACTIVATEAPP = 0x001C;
+
+    [DllImport("dwmapi.dll")]
+    public static extern int DwmExtendFrameIntoClientArea(IntPtr hWnd, ref MARGINS pMarInset);
+
+    [DllImport("dwmapi.dll")]
+    public static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr, ref int attrValue, int attrSize);
+
+    [DllImport("dwmapi.dll")]
+    public static extern int DwmIsCompositionEnabled(ref int pfEnabled);
+
+    [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+    private static extern IntPtr CreateRoundRectRgn
+    (
+        int nLeftRect,
+        int nTopRect,
+        int nRightRect,
+        int nBottomRect,
+        int nWidthEllipse,
+        int nHeightEllipse
+    );
+
+    public struct MARGINS
+    {
+        public int leftWidth;
+        public int rightWidth;
+        public int topHeight;
+        public int bottomHeight;
+    }
+
+    protected override CreateParams CreateParams
+    {
+        get
         {
-            ControlPaint.DrawBorder(e.Graphics, ClientRectangle, Color.Gray, ButtonBorderStyle.Solid);
+            m_aeroEnabled = CheckAeroEnabled();
+            CreateParams cp = base.CreateParams;
+            if (!m_aeroEnabled)
+                cp.ClassStyle |= CS_DROPSHADOW; return cp;
         }
     }
+
+    private bool CheckAeroEnabled()
+    {
+        if (Environment.OSVersion.Version.Major >= 6)
+        {
+            int enabled = 0; DwmIsCompositionEnabled(ref enabled);
+            return (enabled == 1) ? true : false;
+        }
+        return false;
+    }
+
+    protected override void WndProc(ref Message m)
+    {
+        switch (m.Msg)
+        {
+            case WM_NCPAINT:
+                if (m_aeroEnabled)
+                {
+                    var v = 2;
+                    DwmSetWindowAttribute(this.Handle, 2, ref v, 4);
+                    MARGINS margins = new MARGINS()
+                    {
+                        bottomHeight = 1,
+                        leftWidth = 0,
+                        rightWidth = 0,
+                        topHeight = 0
+                    }; DwmExtendFrameIntoClientArea(this.Handle, ref margins);
+                }
+                break;
+            default: break;
+        }
+        base.WndProc(ref m);
+        if (m.Msg == WM_NCHITTEST && (int)m.Result == HTCLIENT) m.Result = (IntPtr)HTCAPTION;
+    }
+
+    /// <summary>
+    /// Border for form.
+    /// </summary>
+    protected override void OnPaint(PaintEventArgs e)
+    {
+        ControlPaint.DrawBorder(e.Graphics, ClientRectangle, Color.Gray, ButtonBorderStyle.Solid);
+    }
+    #endregion
 }
