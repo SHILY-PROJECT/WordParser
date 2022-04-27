79185550c454e302ae41589e4399336ba990e0df
Ilya Shimbuev <iliyarock@gmail.com>
2022-04-27 14:09:10 +0300

    -Добавлен 'EqualityComparer' для сравнения слов

--- src/Core/Models/WordModel.cs
+++ src/Core/Models/WordModel.cs
@@ -1,17 +1,7 @@
 ﻿namespace WordParser.Core.Models;
 
-internal record WordModel : IEqualityComparer<WordModel>
+internal record WordModel
 {
     public string Word { get; init; } = string.Empty;
     public int Quantity { get; init; }
-
-    bool IEqualityComparer<WordModel>.Equals(WordModel? firstWordModel, WordModel? secondWordModel)
-    {
-        return firstWordModel?.Word?.Equals(secondWordModel?.Word, StringComparison.OrdinalIgnoreCase) ?? throw new Exception();
-    }
-
-    int IEqualityComparer<WordModel>.GetHashCode(WordModel wordModel)
-    {
-        return HashCode.Combine(Word.GetHashCode(), wordModel.Word);
-    }
 }
