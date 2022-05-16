a91c1cf59bb272bb46278c6ae245b6d6a5ab37a4
Ilya Shimbuev <iliyarock@gmail.com>
2022-05-16 23:03:02 +0300

    -Убрана тень формы ожидания (багует)

--- src/Core/Forms/WaitForm.cs
+++ src/Core/Forms/WaitForm.cs
@@ -6,8 +6,6 @@ internal partial class WaitForm : Form
     {
         InitializeComponent();
         RegisterFormEvents();
-
-        Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 25, 25));
     }
 
     private void RegisterFormEvents()
@@ -22,96 +20,8 @@ internal partial class WaitForm : Form
             Owner.Location.Y + Owner.Height / 2 - this.Height / 2);
     }
 
-    #region Shadow of form & Border for form.
-    private const int WM_NCHITTEST = 0x84;
-    private const int HTCLIENT = 0x1;
-    private const int HTCAPTION = 0x2;
-
-    private bool m_aeroEnabled;
-
-    private const int CS_DROPSHADOW = 0x00020000;
-    private const int WM_NCPAINT = 0x0085;
-    private const int WM_ACTIVATEAPP = 0x001C;
-
-    [DllImport("dwmapi.dll")]
-    public static extern int DwmExtendFrameIntoClientArea(IntPtr hWnd, ref MARGINS pMarInset);
-
-    [DllImport("dwmapi.dll")]
-    public static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr, ref int attrValue, int attrSize);
-
-    [DllImport("dwmapi.dll")]
-    public static extern int DwmIsCompositionEnabled(ref int pfEnabled);
-
-    [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
-    private static extern IntPtr CreateRoundRectRgn
-    (
-        int nLeftRect,
-        int nTopRect,
-        int nRightRect,
-        int nBottomRect,
-        int nWidthEllipse,
-        int nHeightEllipse
-    );
-
-    public struct MARGINS
-    {
-        public int leftWidth;
-        public int rightWidth;
-        public int topHeight;
-        public int bottomHeight;
-    }
-
-    protected override CreateParams CreateParams
-    {
-        get
-        {
-            m_aeroEnabled = CheckAeroEnabled();
-            CreateParams cp = base.CreateParams;
-            if (!m_aeroEnabled)
-                cp.ClassStyle |= CS_DROPSHADOW; return cp;
-        }
-    }
-
-    private bool CheckAeroEnabled()
-    {
-        if (Environment.OSVersion.Version.Major >= 6)
-        {
-            int enabled = 0; DwmIsCompositionEnabled(ref enabled);
-            return (enabled == 1) ? true : false;
-        }
-        return false;
-    }
-
-    protected override void WndProc(ref Message m)
-    {
-        switch (m.Msg)
-        {
-            case WM_NCPAINT:
-                if (m_aeroEnabled)
-                {
-                    var v = 2;
-                    DwmSetWindowAttribute(this.Handle, 2, ref v, 4);
-                    MARGINS margins = new MARGINS()
-                    {
-                        bottomHeight = 1,
-                        leftWidth = 0,
-                        rightWidth = 0,
-                        topHeight = 0
-                    }; DwmExtendFrameIntoClientArea(this.Handle, ref margins);
-                }
-                break;
-            default: break;
-        }
-        base.WndProc(ref m);
-        if (m.Msg == WM_NCHITTEST && (int)m.Result == HTCLIENT) m.Result = (IntPtr)HTCAPTION;
-    }
-
-    /// <summary>
-    /// Border for form.
-    /// </summary>
     protected override void OnPaint(PaintEventArgs e)
     {
         ControlPaint.DrawBorder(e.Graphics, ClientRectangle, Color.Gray, ButtonBorderStyle.Solid);
     }
-    #endregion
 }
